b4adfd94dcbe13bd0ae19d9a101d41a1bb344507
ehteshamawan1 <ehtesham.awan.ea@gmail.com>
2023-06-16 18:20:28 +0500

    committed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pilgrim_assist/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SplashScreen();
+    return const SplashScreen();
   }
 }
