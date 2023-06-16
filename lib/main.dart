f817029f8c12395af33f9627bb42742f73a90292
ehteshamawan1 <ehtesham.awan.ea@gmail.com>
2023-06-16 18:18:28 +0500

    committed

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,11 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen();
+  }
+}
