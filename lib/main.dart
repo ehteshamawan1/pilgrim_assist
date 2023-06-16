50b58d4d602dfc71dd121ce9dadfc44a22df8c45
ehteshamawan1 <ehtesham.awan.ea@gmail.com>
2023-06-16 18:46:18 +0500

    committed

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SplashScreen();
+    return const MaterialApp(
+      title: 'Pilgrim Assist',
+      home: SplashScreen(),
+    );
   }
 }
